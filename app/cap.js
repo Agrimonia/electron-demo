f834a2b966ea9a07146e9069c807e80bbd17251a
Yongcong Lin <646677472@qq.com>
2018-06-03 14:43:59 +0800

    lowdb => stdout

---
+++ app/cap.js
@@ -0,0 +1,57 @@
+// lowdb
+/*
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+db.defaults({ records: [] })
+  .write()
+*/
+const { Cap, decoders } = require('cap');
+const PROTOCOL = decoders.PROTOCOL;
+
+const c = new Cap();
+const device = Cap.findDevice(getIP());
+const filter = 'tcp and dst port 80';
+const bufSize = 10 * 1024 * 1024;
+const buffer = Buffer.alloc(65535);
+
+const linkType = c.open(device, filter, bufSize, buffer);
+
+c.setMinBytes && c.setMinBytes(0);
+
+c.on('packet', function (nbytes, trunc) {
+  if (linkType === 'ETHERNET') {
+    let ret = decoders.Ethernet(buffer);
+
+    if (ret.info.type === PROTOCOL.ETHERNET.IPV4) {
+      ret = decoders.IPV4(buffer, ret.offset);
+      if (ret.info.protocol === PROTOCOL.IP.TCP) {
+        let datalen = ret.info.totallen - ret.hdrlen;
+        ret = decoders.TCP(buffer, ret.offset);
+        datalen -= ret.hdrlen;
+        let host = buffer.toString('binary', ret.offset, ret.offset + datalen).match(/(?<=Host: )(.*)/);
+        if(host != null) {
+          console.log(host[1]);
+          /*
+          db.get("records")
+            .push({ Time: data,"host": host[1] })
+            .write();
+          */
+        }
+      }
+    }
+  }
+});
+// 自动获得本地IP
+function getIP() {
+  const os = require('os');
+  let IPv4;
+  for (var i = 0; i < os.networkInterfaces().en0.length; i++) {
+    if (os.networkInterfaces().en0[i].family == 'IPv4') {
+      IPv4 = os.networkInterfaces().en0[i].address;
+    }
+  }
+  return IPv4;
+}
--- app/main.js
+++ app/main.js
@@ -1,6 +1,8 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
+const fs = require('fs');
+const child_process = require('child_process');
 
 const isDevelopment = true;
 if (isDevelopment) {
@@ -11,8 +13,12 @@ if (isDevelopment) {
   });
 }
 let mainWindow;
+let cap;
 function createWindow() {
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow = new BrowserWindow({ 
+    height: 600, // 高
+    width: 800, // 宽
+  });
   // 启动文件入口，如 index.html
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
@@ -27,10 +33,21 @@ function createWindow() {
   });
 }
 // 加载就绪
-app.on('ready', createWindow);
+app.on('ready', () => {
+  createWindow();
+  cap = child_process.spawn('node', ['./app/cap.js']);
+  cap.on('close', function (code) {
+    console.log('child process exited with code ' + code);
+  });
+  cap.stdout.on('data', function (data) {
+    console.log('stdout: ' + data);
+  });
+});
 // 监听所有窗口关闭的事件
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
+    child_process.exit();
+    console.log("cap exit!");
     app.quit();
   }
 });
